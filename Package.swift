8606da96cb544e89ca34ace782b3703768b35c81
Aliaksandr Bialiauski <aliaksandr@bialiauski.com>
2021-03-10 20:57:45 +0300

    Xcode 12

--- Package.swift
+++ Package.swift
@@ -1,27 +1,29 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
-    name: "Informator",
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10)
-    ],
-    products: [
-        .library(
-            name: "Informator",
-            targets: ["Informator"]),
-    ],
-    targets: [
-        .target(
-            name: "Informator",
-            dependencies: []),
-        .testTarget(
-            name: "InformatorTests",
-            dependencies: ["Informator"]),
-    ],
-    swiftLanguageVersions: [.v5]
+  name: "Informator",
+  platforms: [
+    .macOS(.v10_12),
+    .iOS(.v10),
+    .tvOS(.v10)
+  ],
+  products: [
+    .library(
+      name: "Informator",
+      targets: ["Informator"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "Informator",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "InformatorTests",
+      dependencies: ["Informator"]
+    ),
+  ],
+  swiftLanguageVersions: [.v5]
 )
