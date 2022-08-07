ff98bc0d14d737c95b7e82abfaa568730f81bf00
Aliaksandr Bialiauski <aliaksandr.bialiauski@mapbox.com>
2022-08-07 15:54:57 +0300

    Add convenience method

--- Sources/Informator/Messageable/Messageable+Error.swift
+++ Sources/Informator/Messageable/Messageable+Error.swift
@@ -3,13 +3,15 @@ import Foundation
 /// Wraps error to be compatible with Informator
 public struct MessageableError: Messageable {
   private let error: Swift.Error
+  private let title: String
 
-  public init(from error: Swift.Error) {
-    self.error = error
+  public init(from error: Swift.Error, title: String? = nil) {
+      self.error = error
+      self.title = title ?? NSLocalizedString("Error", comment: "")
   }
 
   public var messageTitle: String {
-    return NSLocalizedString("Error", comment: "")
+    title
   }
 
   public var messageBody: String {
@@ -40,8 +42,20 @@ public struct Message: Messageable {
 // MARK: - Swift 5.5 features
 #if swift(>=5.5)
 extension Messageable where Self == MessageableError {
-  public static func error(from error: Error) -> MessageableError {
-      .init(from: error)
+    public static func error(
+        from error: Error,
+        title: String?
+    ) -> MessageableError {
+      .init(from: error, title: title)
+  }
+}
+
+extension Messageable where Self == Message {
+    public static func message(
+        title: String,
+        body: String
+    ) -> Message {
+      .init(title: title, body: body)
   }
 }
 #endif
